e587aadcb40570cd2a83faa420a09514cc33097a
Alex Atamanova <alex@example.com>
2022-11-29 18:02:27 +0400

    add new tests and correct exicted; add test case

--- tests/authorization_test.py
+++ tests/authorization_test.py
@@ -1,9 +1,10 @@
 from page.authorization_page import AuthorizationPage
-from generator.generator import generator_user
 
 
 class TestAuthorizationClass:
     AUTHORIZATION_URL = "https://www.saucedemo.com/"
+    R_USERNAME = 'best_guy'
+    R_PASSWORD = 'sunshine'
 
     def test_standard_user(self, driver):
         authorization_page = AuthorizationPage(driver, self.AUTHORIZATION_URL)
@@ -29,12 +30,16 @@ class TestAuthorizationClass:
         received_url = authorization_page.fill_fields_and_submit('performance_glitch_user', 'secret_sauce')
         assert received_url != self.AUTHORIZATION_URL
 
-    def test_generator_user(self, driver):
+    def test_with_wrong_user(self, driver):
         authorization_page = AuthorizationPage(driver, self.AUTHORIZATION_URL)
         authorization_page.open()
-        person = generator_user()
-        print(person.email, person.password)
-        received_url = authorization_page.fill_fields_and_submit(person.email, person.password)
+        received_url = authorization_page.fill_fields_and_submit(self.R_USERNAME, self.R_PASSWORD)
+        assert received_url == self.AUTHORIZATION_URL
+
+    def test_user_without_username_and_password(self, driver):
+        authorization_page = AuthorizationPage(driver, self.AUTHORIZATION_URL)
+        authorization_page.open()
+        received_url = authorization_page.fill_fields_and_submit('', '')
         assert received_url == self.AUTHORIZATION_URL
 
     def test_user_without_password(self, driver):
@@ -43,14 +48,22 @@ class TestAuthorizationClass:
         received_url = authorization_page.fill_fields_and_submit('standard_user', '')
         assert received_url == self.AUTHORIZATION_URL
 
+    def test_user_with_wrong_password(self, driver):
+        authorization_page = AuthorizationPage(driver, self.AUTHORIZATION_URL)
+        authorization_page.open()
+        received_url = authorization_page.fill_fields_and_submit('standard_user', self.R_PASSWORD)
+        assert received_url == self.AUTHORIZATION_URL
+
     def test_user_without_username(self, driver):
         authorization_page = AuthorizationPage(driver, self.AUTHORIZATION_URL)
         authorization_page.open()
         received_url = authorization_page.fill_fields_and_submit('', 'secret_sauce')
         assert received_url == self.AUTHORIZATION_URL
 
-    def test_user_without_username_and_password(self, driver):
+    def test_user_with_wrong_username(self, driver):
         authorization_page = AuthorizationPage(driver, self.AUTHORIZATION_URL)
         authorization_page.open()
-        received_url = authorization_page.fill_fields_and_submit('', '')
+        received_url = authorization_page.fill_fields_and_submit(self.R_USERNAME, 'secret_sauce')
         assert received_url == self.AUTHORIZATION_URL
+
+
